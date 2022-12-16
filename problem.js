ed4671e9f076f6e5816a12d9fef6570947d195c6
JEON <wjsrudals411@gmail.com>
2022-12-16 15:35:50 +0900

    1 commit

---
+++ problem.js
@@ -0,0 +1,19 @@
+    let p1 = {
+        
+    }
+
+    let problems = {
+        prblib: [{   
+        kor: "이것은 연필입니다",
+        eng: "this is a pencil"   
+      },
+      
+      {kor: "안녕하세요 제이름은 전경민입니다",
+       eng : "hello my name is gyungmin"},
+
+      {kor: "한글내용 3",
+       eng: "english3"}
+    
+    ]
+    }
+    
